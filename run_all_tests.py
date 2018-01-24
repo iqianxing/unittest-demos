978116f994fa7b3f57701a71be2d5b0dd95e7550
iqianxing <qianxingqianli@qq.com>
2018-01-24 23:37:36 +0800

    add testrunner

---
+++ run_all_tests.py
@@ -0,0 +1,15 @@
+import unittest
+import os
+
+
+def all_case():
+    case_path = os.path.dirname(__file__) + "case"
+    discover = unittest.defaultTestLoader.discover(
+        case_path, pattern='test*.py', top_level_dir=None)
+    print discover
+    return discover
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(all_case())
