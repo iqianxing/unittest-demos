3cf7d1ca480b719d390d87b0c3735c84d9441181
iqianxing <qianxingqianli@qq.com>
2018-02-19 09:39:43 +0800

    remove some test

--- pytest/test_abc.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def func(x, y):
-    return x + y
-
-
-def test_answer():
-    assert func(3, 2) == 5
--- pytest/test_git.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-def caculater(x, y):
-    return x + y
-
-def test_caculater():
-    assert caculater(3, 2) == 5
